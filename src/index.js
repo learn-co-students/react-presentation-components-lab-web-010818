cf9f7865949b48c619026260c395f7d22530c0e2
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-14 15:52:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const addKill = (event) => {
+  // alert("KILL!")
+  let kill = document.createElement('p').innerText = 'KILL'
+  event.target.parentNode.innerHTML += kill
+
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={addKill} />
   </div>,
   document.getElementById('root')
 );
